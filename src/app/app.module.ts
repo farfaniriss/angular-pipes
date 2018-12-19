8933e3775634d2f08f01b6030c425907a98c788b
farfaniriss <iris.farfan.c@gmail.com>
2018-12-19 18:22:38 -0500

    some useful pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { LOCALE_ID  } from '@angular/core'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomseguroPipe,
+    ContrasenaPipe,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
